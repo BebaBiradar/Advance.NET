51c0867eddf751336662f31fe8409b7905666591
BebaBiradar <107671062+BebaBiradar@users.noreply.github.com>
2022-08-30 10:29:34 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,95 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FileIoDemo
+namespace SerializationDemo
 {
-    //Using DirectoryInfo & file info class
     class Program
     {
-        static void CreateFolder()
+        static void Main(string[] args)
         {
-            string path = @"E:\Advance C sharpe\TestFolder";
-            if (Directory.Exists(path))
-            {
-                Console.WriteLine("Folder is already exits");
-            }
-            else
-            {
-                Directory.CreateDirectory(path);
-                Console.WriteLine("Folder is created");
-            }
-        }
-        static void FileCreate()
-        {
-            string path = @"E:\Advance C sharpe\FileFolder\TestFile.txt";
-            if (Directory.Exists(path))
-            {
-                Console.WriteLine("File folder is already exits");
-            }
-            else
-            {
-                Directory.CreateDirectory(path);
-                Console.WriteLine("File folder is Created");
-            }
-        }
-        public class Course
-        {
-            int id;
-            string name;
-            int fees;
-
-            public int Id { get => id; set => id = value; }
-            public string Name { get => name; set => name = value; }
-            public int Fees { get => fees; set => fees = value; }
-        }
-        class AssignmentBinryWrRd
-        {
-            static void FileToWrite(Course cs)
-            {
-                try
-                {
-                    FileStream fs = new FileStream(@"E:\Advance C sharpe\TestFolder\TextFile2", FileMode.Create, FileAccess.Write);
-                    BinaryWriter bw = new BinaryWriter(fs);
-                    bw.Write(cs.Id);
-                    bw.Write(cs.Name);
-                    bw.Write(cs.Fees);
-                    fs.Close();
-                    bw.Close();
-                    Console.WriteLine("File is create");
-                }
-
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
-            }
-            static void ReadFromFile()
-            {
-                try
-                {
-                    FileStream fs = new FileStream(@"E:\Advance C sharpee\TestFolder\TextFile1", FileMode.Open, FileAccess.Read);
-                    BinaryReader br = new BinaryReader(fs);
-                    Console.WriteLine(br.ReadInt32());
-                    Console.WriteLine(br.ReadString());
-                    Console.WriteLine(br.ReadInt32());
-
-                    br.Close();
-                    fs.Close();
-                    //Console.WriteLine("File is open");
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
-            }
-            static void Main(string[] args)
-            {
-                Course cs = new Course { Id = 1, Name = "Riya", Fees = 10000 };
-                FileToWrite(cs);
-                ReadFromFile();
-                Console.ReadLine();
-            }
         }
     }
-}
 }
---
+++ Serializable_demo.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Xml.Serialization;
+
+namespace SerializationDemo
+{
+
+    [Serializable]
+    public class Student
+    {
+        public int rno { get; set; }
+        public String name { get; set; }
+        public int percentage { get; set; }
+    }
+    class Serializable_demo
+    {
+        static void BinarySerilizationWrite(Student stud)
+        {
+            try
+            {
+                FileStream fs = new FileStream(@"D:\FileFolder\BinaryFile.dat", FileMode.Create, FileAccess.Write);
+                BinaryFormatter bf = new BinaryFormatter();
+                bf.Serialize(fs, stud);
+                Console.WriteLine("Binary data Added....");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        static void BinarySerilizationRead()
+        {
+            try
+            {
+                FileStream fs = new FileStream(@"D:\FileFolder\BinaryFile.dat", FileMode.Open, FileAccess.Read);
+                BinaryFormatter bf = new BinaryFormatter();
+                Student stud = (Student)bf.Deserialize(fs);
+                Console.WriteLine(stud.rno);
+                Console.WriteLine(stud.name);
+                Console.WriteLine(stud.percentage);
+                fs.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        Student stud = new Student { rno = 101, name = "Ashwini", percentage = 89 };
+        // CreateFolder();
+        // CreateFile();
+
+         BinarySerilizationWrite(stud);
+        BinarySerilizationRead();
+    }
+}
